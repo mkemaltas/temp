7ce8c83232d6a5d11cf307b7b4489d33e32a12eb
Mustafa Kemal Taş <mkemaltas@sabanciuniv.edu>
2016-08-23 17:46:35 +0300

    Başlıyoruz beybi

--- app/src/main/java/com/example/mkemaltas/yapiyoz/BaseActivity.java
+++ app/src/main/java/com/example/mkemaltas/yapiyoz/BaseActivity.java
@@ -8,6 +8,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+        //setContentView(R.layout.fragment_login);
     }
 }
---
+++ app/src/main/java/com/example/mkemaltas/yapiyoz/notpaylas/ui/LoginFragment.java
@@ -0,0 +1,15 @@
+package com.example.mkemaltas.yapiyoz.notpaylas.ui;
+
+import android.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by mkemaltas on 23.08.2016.
+ */
+
+public class LoginFragment  extends Fragment{
+
+    private FirebaseAuth mAuth;
+
+}
